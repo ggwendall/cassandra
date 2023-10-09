1f5912c2bac694c6b94c5101c963fa01396c8593
Gwendal <48108275+ggwendall@users.noreply.github.com>
2023-10-09 15:26:58 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from cassandra.cluster import Cluster
-if __name__ == "__main__":
-    cluster = Cluster(['localhost'],port=9042)
-    session = cluster.connect('resto',wait_for_all_pools=True)
-    # session.execute('USE resto')
-    rows = session.execute('SELECT * FROM restaurant')
-    for row in rows:
-        print(row)
